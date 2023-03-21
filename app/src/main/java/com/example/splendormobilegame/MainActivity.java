ca917a397092baa158994ad2349d4dd59457f8bd
Kamil Magdziarz <kmkmagdziarz@gmail.com>
2023-03-21 19:53:47 +0100

    Connect to server + message example

--- app/src/main/java/com/example/splendormobilegame/MainActivity.java
+++ app/src/main/java/com/example/splendormobilegame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.splendormobilegame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -10,8 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.splendormobilegame.databinding.ActivityMainActivityBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
+
+public class MainActivity extends AppCompatActivity implements Serializable {
     private ActivityMainActivityBinding binding;
+    private WebSocketClient webSocketClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +34,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
         //Setup buttons
         setupButtons();
+        //Join Server
+        createWebSocketClient();
+    }
+
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            // Connect to server
+            uri = new URI("ws://20.199.10.79:8887");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Log.i("WebSocket", "Session is starting");
+                //Example message
+                webSocketClient.send("{\"messageContextId\":\"80bdc250-5365-4caf-8dd9-a33e709a0116\",\"type\":\"CreateRoom\",\"data\":{\"userDTO\":{\"id\":\"f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454\",\"name\":\"James\"},\"roomDTO\":{\"name\":\"TajnyPokoj\",\"password\":\"kjashjkasd\"}}}");
+            }
+
+            @Override
+            public void onTextReceived(String s) {
+                Log.i("WebSocket", "Message received");
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            binding.gameTitleTextView.setText(message);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                Log.i("WebSocket", "Closed ");
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
     }
 
     private void setupButtons() {
