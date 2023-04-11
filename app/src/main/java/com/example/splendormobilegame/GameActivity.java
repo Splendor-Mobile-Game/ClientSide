8129aabbf4dd8c17349138f1c18757c76b03dbfe
Kamil Magdziarz <kmkmagdziarz@gmail.com>
2023-04-11 11:38:49 +0200

    Added option buttons + fixed small theme bug + changed some designs

--- app/src/main/java/com/example/splendormobilegame/GameActivity.java
+++ app/src/main/java/com/example/splendormobilegame/GameActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +18,14 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityGameActivityBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
        // binding.gameActivityConstraintLayout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
         setupSideBar();
+        setupButtons();
     }
     public void setupSideBar(){
         binding.sideBarButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +36,31 @@ public class GameActivity extends AppCompatActivity {
                 TransitionManager.beginDelayedTransition(binding.gameActivityConstraintLayout,new AutoTransition());
                 binding.pointsCardView.setVisibility(pointCardVisible);
                 binding.otherPlayerCardView.setVisibility(otherCardsVisible);
+            }
+        });
+
+
+    }
+    public void setupButtons(){
+        //Button used for taking points
+        binding.takePointsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //button used for reserving cards
+        binding.reserveCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //button used for buying card
+        binding.buyCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
