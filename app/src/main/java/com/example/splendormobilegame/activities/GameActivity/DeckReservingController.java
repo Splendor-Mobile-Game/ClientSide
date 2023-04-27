2de9a95d70b118d5671226f8ff976c3e47c7b82d
michalwisniewski3101 <michal.wisniewski3101@gmail.com>
2023-04-27 12:25:45 +0200

    Revert "Implement `DeckReservingController`"
    This reverts commit 70b739fab476c3bb3f0c4d53f80dafa91431b5ec.

--- app/src/main/java/com/example/splendormobilegame/activities/GameActivity/DeckReservingController.java
+++ app/src/main/java/com/example/splendormobilegame/activities/GameActivity/DeckReservingController.java
@@ -4,19 +4,11 @@ import android.util.Log;
 
 import com.example.splendormobilegame.Controller;
 import com.example.splendormobilegame.websocket.UserReaction;
-import com.example.splendormobilegame.websocket.ReactionUtils;
 import com.github.splendor_mobile_game.websocket.communication.ServerMessage;
 import com.github.splendor_mobile_game.websocket.communication.UserMessage;
 import com.github.splendor_mobile_game.websocket.response.ErrorResponse;
-import com.github.splendor_mobile_game.websocket.handlers.UserRequestType;
-
-
-
-
 
 import java.util.UUID;
-
-import javax.smartcardio.Card;
 
 public class DeckReservingController<T extends GameActivity> extends Controller {
 
@@ -40,14 +32,6 @@ public class DeckReservingController<T extends GameActivity> extends Controller 
     private void sendRequestToReserve(UUID cardUuid) {
         // TODO Compose up the message to the server
         // TODO Send the message
-
-        Card card= getCardByUuid(cardUuid);
-        
-        MakeReservationFromDeck.DataDTO dataDTO = new MakeReservationFromDeck.DataDTO(Model.getUserUuid(),card.getCardTier());
-
-        UserMessage userMessage = new UserMessage(UUID.randomUUID(), UserRequestType.MAKE_RESERVATION_FROM_DECK, dataDTO);
-        CustomWebSocketClient.getInstance().send(userMessage);
-
     }
 
     public ReservationFromDeckMessageHandler getReservationFromDeckMessageHandler() {
@@ -60,24 +44,11 @@ public class DeckReservingController<T extends GameActivity> extends Controller 
         public UserMessage react(ServerMessage serverMessage) {
             Log.i("UserReaction", "Entered ReservationFromDeckMessageHandler react method");
 
-            
-            MakeReservationFromDeck.ResponseData responseData = (MakeReservationFromDeck.ResponseData) ReactionUtils.getResponseData(serverMessage, MakeReservationFromDeck.ResponseData.class);
-
-
-           
-
-            reserveCard(responseData.userUuid, responseData.card);
-            
-
-
+            // TODO Update the model
             // TODO Update the view via `gameActivity` or other objects given in constructor
 
             // If this message pertains to me, it means I requested it, indicating that I have taken my action during my turn.
             // Therefore, I need to end my turn.
-
-
-            activity.showToast("User "+getUserByUuid(responseData.userUuid).getName()+" reserved card from deck "+responseData.card.getCardTier());
-
             // Perhaps it was not the best decision to require the user to manually end their turn.
             // The server should handle this automatically.
             DeckReservingController.this.turnController.endTurn();
